1fb9fafe3107f0e570c4a66a888ce52c2299554c
omarnabulsi16 <31221375+omarnabulsi16@users.noreply.github.com>
2020-03-09 19:11:35 -0700

    Add files via upload

---
+++ cs-sketch.js
@@ -0,0 +1,116 @@
+// cs-sketch.js; P5 key animation fcns.  // CF p5js.org/reference
+// Time-stamp: <2020-02-17 19:15:08 Chuck Siska>
+
+// ============================================================ Mods ====
+// to 2020-02-10 16:42:24: add btns.
+// to 2020-02-09 16:55:21: add btn onclick exported fn
+// to 2020-02-10 17:22:23: log btn onclick
+
+// Make global g_canvas JS 'object': a key-value 'dictionary'.
+var g_canvas; // JS Global var, w canvas size info.
+var g_frame_cnt; // Setup a P5 display-frame counter, to do anim
+var g_frame_mod; // Update ever 'mod' frames.
+var g_stop; // Go by default.
+var g_cnv;   // To hold a P5 canvas.
+var g_button; // btn
+var g_button2; // btn
+
+var g_l4job = { id:1 }; // Put Lisp stuff for JS-to-access in ob; id to make ob.
+
+function do_btn( )
+{ // grab code from csu\assets\js\js+p5+editbox
+
+    // Creates an <input></input> element in the DOM for text input.
+    // Use g_input.size() to set the display length of the box.
+    g_input = createInput( ); // Create input textbox; get via "contentx = g_input.value();"
+    g_input.position(  20, 30 );
+    g_button = createButton( "Submit" );
+    g_button.id( "btn" ); //Add for P5 btn onclick
+    g_button.position( 160, 30 );
+    text( "Enter your name.", 20, 20 );
+
+    g_button2 = createButton( "Save Image" );
+    g_button2.position( 20, 60 );
+    g_button2.mousePressed( save_image ); // the callback
+}
+
+function save_image( ) // btn
+{
+    save('myCanvas-' + g_frame_cnt +  '.jpg');
+}
+
+function setup() // P5 Setup Fcn
+{
+    console.log( "Beg P5 setup =====");
+    console.log( "@: log says hello from P5 setup()." );
+    g_canvas = { cell_size:10, wid:64, hgt:48 };
+    g_frame_cnt = 0; // Setup a P5 display-frame counter, to do anim
+    g_frame_mod = 24; // Update ever 'mod' frames.
+    g_stop = 0; // Go by default.
+
+    let sz = g_canvas.cell_size;
+    let width = sz * g_canvas.wid;  // Our 'canvas' uses cells of given size, not 1x1.
+    let height = sz * g_canvas.hgt;
+    g_cnv = createCanvas( width, height );  // Make a P5 canvas.
+    console.log( "@: createCanvas()." );
+    draw_grid( 10, 50, 'white', 'yellow' );
+    do_btn( ); // 
+
+    console.log( "End P5 setup =====");
+}
+
+var g_bot = { dir:3, x:20, y:20, color:100 }; // Dir is 0..7 clock, w 0 up.
+var g_box = { t:1, hgt:47, l:1, wid:63 }; // Box in which bot can move.
+
+function csjs_get_pixel_color_sum( rx, ry )
+{
+    let acolors = get( rx, ry ); // Get pixel color [RGBA] array.
+    let sum = acolors[ 0 ] + acolors[ 1 ] + acolors[ 2 ]; // Sum RGB.
+    //dbg console.log( "color_sum = " + sum );
+    return sum;
+}
+
+function draw_update()  // Update our display.
+{
+    console.log( "Call g_l4job.draw_fn" );
+    g_l4job.draw_fn( );
+}
+
+function draw()  // P5 Frame Re-draw Fcn, Called for Every Frame.
+{
+    ++g_frame_cnt;
+    if (0 == g_frame_cnt % g_frame_mod)
+    {
+        console.log( "g_frame_cnt = " + g_frame_cnt );
+        if (!g_stop) draw_update();
+    }
+}
+
+function keyPressed( )
+{
+    console.log( "@: keyPressed " );
+    g_stop = ! g_stop;
+    if (g_stop) { noLoop(); } else {loop();}
+}
+
+function mousePressed( )
+{
+    console.log( "@: mousePressed " );
+    let x = mouseX;
+    let y = mouseY;
+    //dbg console.log( "mouse x,y = " + x + "," + y );
+    let sz = g_canvas.cell_size;
+    let gridx = round( (x-0.5) / sz );
+    let gridy = round( (y-0.5) / sz );
+    //dbg console.log( "grid x,y = " + gridx + "," + gridy );
+    //dbg console.log( "box wid,hgt = " + g_box.wid + "," + g_box.hgt );
+    g_bot.x = gridx + g_box.wid; // Ensure its positive.
+    //dbg console.log( "bot x = " + g_bot.x );
+    g_bot.x %= g_box.wid; // Wrap to fit box.
+    g_bot.y = gridy + g_box.hgt;
+    //dbg console.log( "bot y = " + g_bot.y );
+    g_bot.y %= g_box.hgt;
+    //dbg console.log( "bot x,y = " + g_bot.x + "," + g_bot.y );
+    console.log( "Call g_l4job.draw_fn for mousePressed" );
+    g_l4job.draw_fn( );
+}
